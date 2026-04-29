4a04bb8929ebbde1fe57de6bd17773dd09934f3a
daelesterio-beep <da.elesterio@spamast.edu.ph>
2026-04-29 18:28:01 +0800

    create booWho function skeleton

--- Boolean Check Function.js
+++ Boolean Check Function.js
@@ -1,15 +1,3 @@
 function booWho(bool) {
-  return typeof bool === "boolean";
+  return bool;
 }
-
-// Tests
-console.log(booWho(true));          // expected: true
-console.log(booWho(false));         // expected: true
-console.log(booWho([1, 2, 3]));     // expected: false
-console.log(booWho([].slice));      // expected: false
-console.log(booWho({ "a": 1 }));    // expected: false
-console.log(booWho(1));             // expected: false
-console.log(booWho(NaN));           // expected: false
-console.log(booWho("a"));           // expected: false
-console.log(booWho("true"));        // expected: false
-console.log(booWho("false"));       // expected: false
